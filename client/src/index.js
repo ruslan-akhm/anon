ead45ef00d290c224a7f0c14afed1d9674ab255e
Ruslan Akhmetshin <ruslan.akhmetshin@keydata.ca>
2022-09-10 19:14:54 -0400

    move router to index

--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import Provider from "./context/Context";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
-		<Provider>
-			<App />
-		</Provider>
+		<BrowserRouter>
+			<Provider>
+				<App />
+			</Provider>
+		</BrowserRouter>
 	</React.StrictMode>
 );
