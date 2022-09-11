8e59f67cfc2f6d0dad9e129febefd42f2573e12a
SlavaChekhunov <slava97@my.yorku.ca>
2022-09-10 21:12:52 -0400

    updated the board

---
+++ client/src/components/mainpage/board/firebase-config.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBJZBUgB67H5bkY9jqOzyeqT9aJuPURVGU",
+  authDomain: "anonproject-49dab.firebaseapp.com",
+  projectId: "anonproject-49dab",
+  storageBucket: "anonproject-49dab.appspot.com",
+  messagingSenderId: "445214118901",
+  appId: "1:445214118901:web:748b3a433b126fe55906bc",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
